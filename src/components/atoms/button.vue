97a73b3b7596d047e8be54b5dc1f4dd84c1918a8
Lenildo-Luan <lenildoluan@gmail.com>
2023-10-11 08:57:27 -0300

    Create components files

---
+++ src/components/atoms/button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="atom-button" @click="$emit('action')">{{ props.content }}</button>
+</template>
+  
+<script setup>
+const props = defineProps({
+    content: String
+})
+</script>
+  
+<style scoped>
+.atom-button {
+    width: 100%;
+    padding: 1rem 0;
+
+    font-size: 1rem;
+    font-weight: bold;
+    text-transform: uppercase;
+}
+</style>
---
+++ src/components/atoms/input.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="atom-input-number-wrapper">
+        <label for="session" class="atom-input-number-label"> {{ label }} </label>
+        <input type="number" name="session" min="1" max="59" :value="value" :disabled="disabled"
+            @input="$emit('updateTime', $event.target.value)" class="atom-input-number" />
+    </div>
+</template>
+  
+<script setup>
+defineProps({
+    label: String,
+    value: Number,
+    disabled: Boolean
+})
+</script>
+  
+<style scoped>
+.atom-input-number-wrapper {
+    width: 100%;
+}
+
+.atom-input-number {
+    width: 100%;
+}
+
+.atom-input-number-label {
+    font-size: 1rem;
+}
+</style>
